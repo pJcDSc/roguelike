e97f9af4803c6c004aea36fcabd69dd1c0a0638d
HusseinKhodor03 <67843437+HusseinKhodor03@users.noreply.github.com>
2020-07-23 13:43:15 -0700

    Arrow Keys Press

---
+++ ArrowKeysPress/ArrowKeysPress.cpp
@@ -0,0 +1,35 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
+int main()
+{
+    int c = 0;
+    while(1)
+    {
+        c = 0;
+
+        switch((c = getch())) {
+        case KEY_UP:
+            cout << endl << "Up" << endl; // Detect Up Arrow Key
+            break;
+        case KEY_DOWN:
+            cout << endl << "Down" << endl; // Detect Down Arrow Key
+            break;
+        case KEY_LEFT:
+            cout << endl << "Left" << endl; // Detect Left Arrow Key
+            break;
+        case KEY_RIGHT:
+            cout << endl << "Right" << endl; // Detect Right Arrow Key
+            break;
+        }
+
+    }
+
+    return 0;
+}
